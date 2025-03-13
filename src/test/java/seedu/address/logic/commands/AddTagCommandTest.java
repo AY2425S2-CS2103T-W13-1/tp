4c34c7bf3945ec3cb954204acaa02b7cd1e7eeaa
Meatsushi64 <tzejenrex@gmail.com>
2025-03-13 22:08:43 +0800

    Add more tests for AddTagCommand and descriptor

--- src/test/java/seedu/address/logic/commands/AddTagCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddTagCommandTest.java
@@ -1,11 +1,14 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.AddTagCommand.AddTagPersonDescriptor;
 import seedu.address.model.AddressBook;
@@ -29,5 +32,50 @@ public class AddTagCommandTest {
         Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
         expectedModel.setPerson(currentPerson, editedPerson);
         assertCommandSuccess(addTagCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_invalidIndex_throwsCommandException() {
+        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
+        Person personForDescriptor = new PersonBuilder().build();
+        AddTagPersonDescriptor descriptor = new AddTagPersonDescriptorBuilder(personForDescriptor).build();
+        AddTagCommand addTagCommand = new AddTagCommand(outOfBoundIndex, descriptor);
+        assertCommandFailure(addTagCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void equals_testAllFields() {
+        Person firstPerson = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        AddTagPersonDescriptor descriptor = new AddTagPersonDescriptorBuilder(firstPerson).build();
+        Person firstPersonOnListDifferentTags = new PersonBuilder().withTags("tester").build();
+        AddTagPersonDescriptor descriptor2 = new AddTagPersonDescriptorBuilder(firstPersonOnListDifferentTags)
+                .build();
+        AddTagCommand addTagCommand = new AddTagCommand(INDEX_FIRST_PERSON, descriptor);
+        AddTagCommand addTagCommandCopy = new AddTagCommand(INDEX_FIRST_PERSON, descriptor);
+        AddTagCommand addTagCommandDifferentIndex = new AddTagCommand(Index.fromOneBased(2), descriptor);
+        AddTagCommand addTagCommandDifferentDescriptor = new AddTagCommand(INDEX_FIRST_PERSON,
+                descriptor2);
+        // same object -> returns true
+        assert(addTagCommand.equals(addTagCommand));
+        // same values -> returns true
+        assert(addTagCommand.equals(addTagCommandCopy));
+        // different types -> returns false
+        assert(!addTagCommand.equals(1));
+        // null -> returns false
+        assert(!addTagCommand.equals(null));
+        // different index -> returns false
+        assert(!addTagCommand.equals(addTagCommandDifferentIndex));
+        // different descriptor -> returns false
+        assert(!addTagCommand.equals(addTagCommandDifferentDescriptor));
+    }
+
+    @Test
+    public void toString_test() {
+        Index index = Index.fromOneBased(1);
+        AddTagPersonDescriptor addTagPersonDescriptor = new AddTagPersonDescriptor();
+        AddTagCommand addTagCommand = new AddTagCommand(index, addTagPersonDescriptor);
+        String expected = AddTagCommand.class.getCanonicalName() + "{index=" + index + ", addTagPersonDescriptor="
+                + addTagPersonDescriptor + "}";
+        assertEquals(expected, addTagCommand.toString());
     }
 }
--- src/test/java/seedu/address/testutil/AddTagPersonDescriptorBuilder.java
+++ src/test/java/seedu/address/testutil/AddTagPersonDescriptorBuilder.java
@@ -1,7 +1,16 @@
 package seedu.address.testutil;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import seedu.address.logic.commands.AddTagCommand.AddTagPersonDescriptor;
+import seedu.address.model.person.Address;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
+import seedu.address.model.person.Phone;
+import seedu.address.model.tag.Tag;
 
 /**
  * A utility class to help with building AddTagPersonDescriptor objects.
@@ -25,6 +34,48 @@ public class AddTagPersonDescriptorBuilder {
         descriptor.setEmail(person.getEmail());
         descriptor.setAddress(person.getAddress());
         descriptor.setTags(person.getTags());
+    }
+
+    /**
+     * Sets the {@code Name} of the {@code AddTagPersonDescriptor} that we are building.
+     */
+    public AddTagPersonDescriptorBuilder withName(String name) {
+        descriptor.setName(new Name(name));
+        return this;
+    }
+
+    /**
+     * Sets the {@code Phone} of the {@code AddTagPersonDescriptor} that we are building.
+     */
+    public AddTagPersonDescriptorBuilder withPhone(String phone) {
+        descriptor.setPhone(new Phone(phone));
+        return this;
+    }
+
+    /**
+     * Sets the {@code Email} of the {@code AddTagPersonDescriptor} that we are building.
+     */
+    public AddTagPersonDescriptorBuilder withEmail(String email) {
+        descriptor.setEmail(new Email(email));
+        return this;
+    }
+
+    /**
+     * Sets the {@code Address} of the {@code AddTagPersonDescriptor} that we are building.
+     */
+    public AddTagPersonDescriptorBuilder withAddress(String address) {
+        descriptor.setAddress(new Address(address));
+        return this;
+    }
+
+    /**
+     * Parses the {@code tags} into a {@code Set<Tag>} and set it to the {@code AddTagPersonDescriptor}
+     * that we are building.
+     */
+    public AddTagPersonDescriptorBuilder withTags(String... tags) {
+        Set<Tag> tagSet = Stream.of(tags).map(Tag::new).collect(Collectors.toSet());
+        descriptor.setTags(tagSet);
+        return this;
     }
     public AddTagPersonDescriptor build() {
         return descriptor;
