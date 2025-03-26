caa67db8ea5aab78f3dea8af16477febb60e8c2a
souledfigurine <freddie1507@gmail.com>
2025-03-26 13:41:43 +0800

    Create DeleteNoteCommand.java

---
+++ src/main/java/seedu/address/logic/commands/DeleteNoteCommand.java
@@ -0,0 +1,66 @@
+package seedu.address.logic.commands;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.person.Person;
+
+import java.util.List;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Deletes the note tagged to the contact identified using it's displayed index from the address book.
+ */
+public class DeleteNoteCommand extends Command {
+
+    public static final String COMMAND_WORD = "deletenote";
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Deletes the note of the person identified by the index number used in the displayed person list.\n"
+            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Example: " + COMMAND_WORD + " 1";
+    public static final String MESSAGE_DELETENOTE_SUCCESS = "Note from %1$s deleted successfully";
+
+    private final Index targetIndex;
+
+    public DeleteNoteCommand(Index targetIndex) {
+        this.targetIndex = targetIndex;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Person> lastShownList = model.getFilteredPersonList();
+
+        if (targetIndex.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+        Person personToDeleteNote = lastShownList.get(targetIndex.getZeroBased());
+        return new CommandResult(String.format(MESSAGE_DELETENOTE_SUCCESS, Messages.format(personToDeleteNote)),
+                false, false, false);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        //instanceof handles nulls
+        if (!(other instanceof DeleteNoteCommand)) {
+            return false;
+        }
+
+        DeleteNoteCommand otherDeleteNoteCommand = (DeleteNoteCommand) other;
+        return targetIndex.equals(otherDeleteNoteCommand.targetIndex);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("targetIndex", targetIndex)
+                .toString();
+    }
+}
