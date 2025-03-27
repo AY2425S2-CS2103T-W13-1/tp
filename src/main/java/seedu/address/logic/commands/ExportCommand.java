54311e2ad7814ba4284af223e52b7302449786ab
wentingchua <wentingchua@gmail.com>
2025-03-27 19:44:10 +0800

    Add usage examples for Mac and Linux

--- src/main/java/seedu/address/logic/commands/ExportCommand.java
+++ src/main/java/seedu/address/logic/commands/ExportCommand.java
@@ -25,7 +25,9 @@ public class ExportCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Exports the address book data at the specified path.\n"
             + "Parameters: FILEPATH (must be a valid file path)\n"
-            + "Example: " + COMMAND_WORD + " C:/Users/username/Desktop/exported_data.json";
+            + "Example (Windows): " + COMMAND_WORD + " C:/Users/username/Desktop/exported_data.json"
+            + "Example (Mac): " + COMMAND_WORD + " /Users/username/Desktop/exported_data.json"
+            + "Example (Linux): " + COMMAND_WORD + " /home/user/desktop/exported_data.json";
     private static final Path SOURCE_JSON_PATH = Paths.get("data/addressbook.json");
     private final Path targetPath;
     private boolean sourcePathValid = true;
