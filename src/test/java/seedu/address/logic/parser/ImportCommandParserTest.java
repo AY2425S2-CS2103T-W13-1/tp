253cda5e87d56160fd8db6777809605e35db1307
wentingchua <wentingchua@gmail.com>
2025-04-05 20:35:09 +0800

    Remove unused import

--- src/test/java/seedu/address/logic/parser/ImportCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ImportCommandParserTest.java
@@ -8,7 +8,6 @@ import java.nio.file.Paths;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.logic.commands.ExportCommand;
 import seedu.address.logic.commands.ImportCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
